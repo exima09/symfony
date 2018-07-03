7ddc8132128a121b1c7d509032897b16f631b667
exima09 <dawidjuszczak01@gmail.com>
2018-07-03 23:32:33 +0200

    add bootstrap and library composer

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,10 +2,10 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class HomeController extends Controller
+class HomeController extends AbstractController
 {
     /**
      * @Route("/home", name="home")
